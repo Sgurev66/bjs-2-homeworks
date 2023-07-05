5df0b632a1eff8c5b5ec0e3ff0e1d73741e3012e
Sgurev66 <119155446+Sgurev66@users.noreply.github.com>
2023-07-06 02:57:17 +0700

    задачи 1 и 2
    решения двух задач базовые концепции

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,27 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+ let d = b**2 - 4*a*c;
+   if (d < 0) {
+    return arr;
+   } else if (d === 0) {
+    x = -b/(2*a);
+    arr.push (x);
+  } else if (d > 0) {
+   x = (-b + Math.sqrt(d) )/(2*a);
+   y = (-b - Math.sqrt(d) )/(2*a);
+   arr.push (x, y);
+  }
   return arr;
-}
+}  
 
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+function calculateTotalMortgage(percent, cont
+  let p = percent / 100 /12;
+  let s = amount - countMonths;
+  let n = countMonths;
+
+  let summPercMonths = s * (p + (p / (((1 + p
+  let total = (summPercMonths * countMonths).
+  return total;
 }
+calculateTotalMortgage (10, 10000, 50000, 12)
