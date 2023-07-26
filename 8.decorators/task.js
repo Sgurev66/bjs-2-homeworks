c667173cb0238f5a4ae03654ee3f46a504848552
Sgurev66 <119155446+Sgurev66@users.noreply.github.com>
2023-07-27 02:18:31 +0700

    Update task.js
    Решение задачи 8 Декораторы

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,45 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
-  
+	let cache = {}
+	return (...args) => {
+		const hash = md5(args);
+		if (hash in cache) {
+			return 'Из кэша: ' + cache[hash];
+
+		}
+		const result = func(...args);
+		if (Object.keys(cache).length > 4) {
+			cache[hash] = result;
+			let extraElem = Object.keys(cache)[0];
+			delete cache[extraElem];
+			return 'Вычисляем: ' + result;
+		}
+		cache[hash] = result;
+		return 'Вычисляем: ' + result;
+	}  
 }
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
-  
+    let timerId = null;
+
+	function resultFunction(...args) {
+		if (!timerId) {
+			func(...args);
+			resultFunction.count++;
+		}
+
+		resultFunction.allCount++;
+
+		clearTimeout(timerId);
+		timerId = setTimeout(() => {
+			func(...args);
+			resultFunction.count++;
+		}, delay)
+	}
+
+	resultFunction.count = 0;
+	resultFunction.allCount = 0;
+
+	return resultFunction; 
 }
