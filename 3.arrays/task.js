8286191b88593aac79d9b4c5344e7ba69e3b10bf
Sgurev66 <119155446+Sgurev66@users.noreply.github.com>
2023-07-27 01:55:31 +0700

    Update task.js
    Решение задачи 3 Массивы

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,23 @@
 function compareArrays(arr1, arr2) {
-  
+    if (arr1.length === arr2.length && arr1.every((value, index) => value === arr2[index])) {
+		return true;
+	} else {
+		return false;
+	} 
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    let total = 0;
+	if (users.length === 0) {
+		return 0
+	}
+	if (gender === "мужской") {
+		let total = users.filter(person => person.gender === "мужской").map(person => person.age);
+		return total.reduce((acc, item) => acc + item) / total.length;
+	} else if (gender === "женский") {
+		total = users.filter(person => person.gender === "женский").map(person => person.age);
+		return total.reduce((acc, item) => acc + item) / total.length;
+	} else {
+		return 0;
+	}
 }
